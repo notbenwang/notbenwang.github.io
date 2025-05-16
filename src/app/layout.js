73e42962bbe08d9a1e004a4324bb9a28efddf180
notbenwang <112118953+notbenwang@users.noreply.github.com>
2025-05-16 10:45:59 -0400

    added opengraph metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,21 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio - Benjamin Wang",
+  title: "Portfolio - Ben Wang",
   description: "I make stuff",
+  openGraph: {
+    title: "Portfolio - Ben Wang",
+    description: "I make stuff",
+    url: "https://notbenwang.github.io",
+    type: "website",
+    image: "https://opengraph.b-cdn.net/production/images/0a318d57-dba7-4187-9f05-20277195c1ca.jpg?token=siEF2KeuUP4fQr6sfmUidstHVeCaqNfbAEJzViC3hlU&height=714&width=1200&expires=33283406395"
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Portfolio - Ben Wang",
+    description: "I make stuff",
+    images: ["https://opengraph.b-cdn.net/production/images/0a318d57-dba7-4187-9f05-20277195c1ca.jpg?token=siEF2KeuUP4fQr6sfmUidstHVeCaqNfbAEJzViC3hlU&height=714&width=1200&expires=33283406395"]
+  }
 };
 
 export default function RootLayout({ children }) {
